d9527014678a349e44a1e1551d32d6cbd5277ac8
Donal Siagian <s.donal@indoliquid.id>
2019-12-04 23:49:35 +0700

    implementation of library and create NanomartApplication.kt also integrate the google service

---
+++ app/src/main/java/id/indoliquid/nanomart/ui/NanomartApplication.kt
@@ -0,0 +1,35 @@
+package id.indoliquid.nanomart.ui
+
+import android.app.Application
+import android.content.Context
+import id.indoliquid.nanomart.R
+import io.github.inflationx.calligraphy3.CalligraphyConfig
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor
+import io.github.inflationx.viewpump.ViewPump
+
+var mContext: Context? = null
+fun getContext(): Context? {
+    return mContext
+}
+
+class NanomartApplication : Application() {
+    var noUsbDebugMode = false
+    var currentCustomerPhone = ""
+
+    override fun onCreate() {
+        super.onCreate()
+        ViewPump.init(
+            ViewPump.builder()
+                .addInterceptor(
+                    CalligraphyInterceptor(
+                        CalligraphyConfig.Builder()
+                            .setDefaultFontPath("fonts/Muli-Regular.ttf")
+                            .setFontAttrId(R.attr.fontPath)
+                            .build()
+                    )
+                )
+                .build())
+        mContext = this
+    }
+}
+
